8798edd4fac4b354d962fddd2745743ea3ff6b14
lxwang <lxwang@gmail.com>
2021-09-21 12:20:32 -0700

    filter out double tweets that have the same content

--- stream.py
+++ stream.py
@@ -4,11 +4,16 @@ import asyncio
 import csv
 import json
 import time
+import re
 
 follow_file = 'follows.json'
 
+must_follows = ['zerohedge']
+
 default_follows = ['CitronResearch', 'muddywatersre', 'Trinhnomics' 'BNONews', 'DeItaone', 'onlyyoontv', 'elonmusk', 'Newsquawk', 'SqueezeMetrics', 'spotgamma', 'WallStJesus',
          'CryptoPanicCom', 'pakpakchicken', 'Trade_The_News', 'disclosetv', '_lakai_', 'hoodietrades',  'zerohedge',  'LiveSquawk',  'FirstSquawk']
+
+news_list = ['disclosetv', 'zerohedge', 'DeItaone']
 
 follow_list = default_follows
 try:
@@ -41,7 +46,7 @@ def addfollow(acct):
         return False
 
 def unfollow(acct):
-    if acct in follow_list:
+    if (acct in follow_list) and (acct not in must_follows):
         follow_list.remove(acct)
         update_follows(follow_list)
         return True
@@ -57,11 +62,18 @@ def update_follows(f):
     followed_ids = get_user_ids(f)
     if stream.running:
         stream.disconnect()
-    stream.filter(follow=followed_ids, is_async=True)
+    #stream.filter(follow=followed_ids, is_async=True)
+
+
 
 class StreamListener(tweepy.StreamListener):
-
+    def __init__(self):
+        super().__init__()
+        # remember the content of the last tweet so that we don't double post
+        self.last_tweet = ""
+    
     def on_status(self, status):
+                
         if status.retweeted:
             return
         # don't post out of context replies
@@ -83,11 +95,28 @@ class StreamListener(tweepy.StreamListener):
 
         creator_id = status.user.id_str
 
-        if status.user.screen_name in follow_list:
+        if status.user.screen_name.upper() in map(str.upper, follow_list):
+            # filter out retweets unless it's from a news source
+            if hasattr(status, 'retweeted_status'):
+                print("rt from", status.user.screen_name )
+                if not(status.user.screen_name.upper() in map(str.upper, news_list)):
+                    return
+
+                # filter out retweets that tweet other accounts being followed
+                regex = r"RT @([^:]+):"
+                matches = re.findall(regex, status.text, re.MULTILINE)
+                if matches[0].upper() in map(str.upper, follow_list):
+                    print('filtered out retweet to', matches[0])
+                    return
+        
             print('{}: {} {}'.format(status.user.screen_name, text, status.source_url))
             tweet_url = 'https://twitter.com/{}/status/{}'.format(status.user.screen_name, status.id_str)
             if len(tweet_queue) == 0 or (not (tweet_url in tweet_queue)):
-                tweet_queue.append(tweet_url)
+                if text.strip() == self.last_tweet.strip():
+                    print("Double tweet by", status.user.screen_name)
+                else:
+                    tweet_queue.append(tweet_url)
+                    self.last_tweet = text
             print("QUEUE: ", tweet_queue)
 
     def on_error(self, status_code):
@@ -117,5 +146,6 @@ stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
 
 
 # start_stream()
+
 
 
